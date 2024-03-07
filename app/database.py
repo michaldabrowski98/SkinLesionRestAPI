d22aea48f6fcac7465002078e8fd6225f2ddbcff
Michał <51337492+michaldabrowski98@users.noreply.github.com>
2024-03-07 17:47:59 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from pydantic_settings import SettingsConfigDict
 
 model_config = SettingsConfigDict(env_file=".env")
 
-SQLALCHEMY_DATABASE_URL = "mysql://avnadmin:AVNS_XpwoxogQlu5fEfXmE0a@mysql-2ddec4d3-mdboxman-6cae.a.aivencloud.com:28995/defaultdb"
+SQLALCHEMY_DATABASE_URL = ""
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
